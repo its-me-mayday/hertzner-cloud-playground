dd63779dac6ee377e4ea2852cb8d74593b4584bc
Luca Maggio <luca.maggio@activadigital.it>
2025-03-10 18:03:36 +0100

    feat(*): init project

---
+++ server.tf
@@ -0,0 +1,16 @@
+resource "hcloud_server" "main-node" {
+  name        = "master-node"
+  image       = "ubuntu-24.04"
+  server_type = "cax11"
+  location    = "fsn1"
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+  network {
+    network_id = hcloud_network.private_network.id
+    ip         = "10.0.1.1"
+  }
+
+  depends_on = [hcloud_network_subnet.private_network_subnet]
+}
